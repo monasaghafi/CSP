9125596c3ccdbbdf32454e3de5fd807655b67473
Mona <96328018+monasaghafi@users.noreply.github.com>
2023-05-27 23:53:40 +0330

    change constraints forms, split to separated classes
    with limited and obvious constraints the solver result to (unsolvable)
    using Not logic for constraints result to display the time but failed to solve

--- KingChoice/KingChoice.py
+++ KingChoice/KingChoice.py
@@ -1,6 +1,17 @@
 from CSP.Problem import Problem
 from CSP.Variable import Variable
-from KingChoice.KingConstraints import *
+from KingChoice.Constraint1 import Constraint1
+from KingChoice.Constraint2 import Constraint2
+from KingChoice.Constraint3 import Constraint3
+from KingChoice.Constraint4 import Constraint4
+from KingChoice.Constraint5 import Constraint5
+from KingChoice.Constraint6 import Constraint6
+from KingChoice.Constraint7 import Constraint7
+from KingChoice.Constraint8 import Constraint8
+from KingChoice.Constraint9 import Constraint9
+from KingChoice.Constraint10 import Constraint10
+from KingChoice.Constraint11 import Constraint11
+
 
 
 class KingChoice(Problem):
@@ -18,13 +29,18 @@ class KingChoice(Problem):
         type4 = Variable[str](['acid', 'poison', 'healing', 'transform', 'invisible'], 'type4')
         type5 = Variable[str](['acid', 'poison', 'healing', 'transform', 'invisible'], 'type5')
 
-        c00 = Const0([color1, color2, color3, color4, color5])
-        c01 = Const0([type1, type2, type3, type4, type5])
-        c1 = Const1([color1, type1])
-        c2 = Const2([color2, type2])
-        c3 = Const3([color3, type3])
-        c4 = Const4([color4, type4])
-        c5 = Const5([color5, type5])
+        c1 = Constraint1([color1, color2, color3, color4, color5])
+        c2 = Constraint1([type1, type2, type3, type4, type5])
+        c3 = Constraint2([type1])
+        c4 = Constraint3([color1])
+        c5 = Constraint4([type2])
+        c6 = Constraint5([color2])
+        c7 = Constraint6([color3])
+        c8 = Constraint7([type3])
+        c9 = Constraint8([type4])
+        c10 = Constraint9([color4])
+        c11 = Constraint10([type5])
+        c12 = Constraint11([color5])
 
-        self.constraints = [c00, c01, c1, c2, c3, c4, c5]
+        self.constraints = [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12]
         self.variables = [color1, color2, color3, color4, color5, type1, type2, type3, type4, type5]
